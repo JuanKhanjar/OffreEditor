a1183a10c21c46df5efaec7cb878b9964fa0214c
jan4ma <jan4ma@outlook.com>
2025-04-14 23:43:12 +0200

    Add project files.

---
+++ OffreEditor/Models/ProductEntry.cs
@@ -0,0 +1,16 @@
+﻿namespace OffreEditor.Models
+{
+    public class ProductEntry
+    {
+        public string? Category { get; set; } 
+        public string? Article { get; set; }
+        public string? Qté { get; set; }
+        public string? Description { get; set; }
+        public string? Détail { get; set; }
+        public string? Pays { get; set; }
+        public string?   Poids { get; set; }
+        public string? Unité { get; set; }
+    }
+
+
+}
